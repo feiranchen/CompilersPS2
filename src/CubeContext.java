79309d88bccaf95c83bdb13d747bfbcabbf272e7
Yunhan Wang <yw559@cornell.edu>
2013-09-22 21:51:51 -0400

    finalv0.1 wyh

--- src/CubeContext.java
+++ src/CubeContext.java
@@ -2,9 +2,9 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class CubeContext {
-	private Map<String,CubeType> mVariables = new HashMap<String,CubeType>();
+	private Map<String,CuType> mVariables = new HashMap<String,CuType>();
 	private Map<String,CubeFunction> mFunctions = new HashMap<String,CubeFunction>();
 
-	public CubeType getVariable(String name) { return mVariables.get(name); }
+	public CuType getVariable(String name) { return mVariables.get(name); }
 	public CubeFunction getFunction(String name) { return mFunctions.get(name); }
 }
