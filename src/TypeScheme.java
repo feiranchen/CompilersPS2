010864a4de9b297de8e9041baee71c74b2f5fb31
Yunhan Wang <yw559@cornell.edu>
2013-09-22 23:25:59 -0400

    final v0.2 before debug

--- src/TypeScheme.java
+++ src/TypeScheme.java
@@ -3,13 +3,34 @@ import java.util.List;
 
 public class TypeScheme extends CuTypeScheme {
 	List<String> data_kc;
-	List<Vvt>    data_tc;
+	List<CuVvt>    data_tc;
 	CuType         data_t;
-	public TypeScheme(List<String> kc, List<Vvt> tc , CuType t){
+	public TypeScheme(List<String> kc, List<CuVvt> tc , CuType t){
 		data_kc=kc;
 		data_tc=tc;
 		data_t=t;
-		super.text=kc.toString()+" "+tc.toString()+" "+t.toString();
+		super.text=listString(data_kc)+" "+listTypes(data_tc)+" : "+t.toString();
 	}
 
+	private String listString(List<String> es) {
+		String s = "< ";
+		for (String e : es) {
+			s += e.toString() + " , ";
+		}
+		int l = s.lastIndexOf(", ");
+		if (l > 0) s = s.substring(0, l);
+		s += ">";
+		return s;
+	}
+	
+	private String listTypes(List<CuVvt> es) {
+		String s = "( ";
+		for (Vvt e : es) {
+			s += e.toString() + " , ";
+		}
+		int l = s.lastIndexOf(", ");
+		if (l > 0) s = s.substring(0, l);
+		s += ")";
+		return s;
+	}
 }
--- src/VClass.java
+++ src/VClass.java
@@ -1,7 +1,21 @@
+import java.util.List;
+
 public class VClass extends CuType {
 	String data_s;
-	public VClass(String s){
+	List<CuType> pt;
+	public VClass(String s, List<CuType> pt){
 		data_s=s;
-		super.text=data_s;
+		this.pt = pt;
+		super.text=data_s + listTypes(pt);
+	}
+	private String listTypes(List<CuType> es) {
+		String s = "< ";
+		for (CuType e : es) {
+			s += e.toString() + " , ";
+		}
+		int l = s.lastIndexOf(", ");
+		if (l > 0) s = s.substring(0, l);
+		s += ">";
+		return s;
 	}
 }
