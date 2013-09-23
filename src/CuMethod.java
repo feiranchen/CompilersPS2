22cdc608efbf7b7c4ff7823e7dc87f45af9a4f02
Yunhan Wang <yw559@cornell.edu>
2013-09-23 19:33:56 -0400

    functxt again

---
+++ src/CuMethod.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CuMethod {
+	protected static <T> String printList(String l, List<T> es, String r, String i) {
+		String s = " "+l + " ";
+		for (T e : es) {
+			s += e.toString() + " "+i + " ";
+		}
+		int j = s.lastIndexOf(i+" ");
+		if (j > 1) s = s.substring(0, j);
+		s += r+" ";
+		return s;
+	}
+	
+	protected static String listFlatten(List<CuStat> cs) {
+		String s="";
+		if (cs == null || cs.size() == 0) return s;
+		for (CuStat t : cs) {
+			if (t instanceof Stats) s+=listFlatten((ArrayList<CuStat>) ((Stats)t).al);
+			else s+= t.toString() + " ";
+		}
+		return s;
+	}
+	
+	protected static void p(String s) {
+		System.out.println(s);
+	}
+}
